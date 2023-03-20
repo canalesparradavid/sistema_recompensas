b578e9f2e702135448563e9ffd48bd070011f7c3
David Canales Parra <canalesparradavid@gmail.com>
2023-03-20 14:59:09 +0100

    Añadidas constantes de Login

--- web/models/User.php
+++ web/models/User.php
@@ -2,6 +2,12 @@
 
 class User{
 
+    // Constantes de Login
+    const LoginOK = 0;
+    const LoginBadPass = 1;
+    const LoginBadEmail = 2;
+    const LoginError = 3;
+
     public static function restoreSession(){
         // TODO
         return new User();
@@ -9,7 +15,7 @@ class User{
 
     public function login($email, $password){
         // TODO
-        return true;
+        return User::LoginOK;
     }
 }
 
