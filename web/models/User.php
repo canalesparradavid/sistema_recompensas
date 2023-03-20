361313fb3dd37417ca1b03c9b7b2aa496c5d7c61
David Canales Parra <canalesparradavid@gmail.com>
2023-03-20 16:49:06 +0100

    Añadido login de usuario

--- web/models/User.php
+++ web/models/User.php
@@ -8,14 +8,53 @@ class User{
     const LoginBadEmail = 2;
     const LoginError = 3;
 
+    // Atributos
+    private $email;
+
     public static function restoreSession(){
         // TODO
         return new User();
     }
 
     public function login($email, $password){
-        // TODO
-        return User::LoginOK;
+        $db = DB::connect();
+
+        $query = "
+            SELECT COUNT(*) n
+            FROM user
+            WHERE
+                email = '$email' AND
+                password = '$password'
+            ;
+        ";
+        $result = $db->query($query);
+        $fila = $result->fetch_array();
+
+        // Si no hay 0 ocurrencias es que los datos estan bien
+        if($fila['n'] != 0){
+            $db->close();
+            $this->email = $email;
+            return User::LoginOK;
+        }
+
+        // Compruebo si el fallo esta en el email
+        $query = "
+            SELECT COUNT(*) n
+            FROM user
+            WHERE
+                email = '$email'
+            ;
+        ";
+        $result = $db->query($query);
+        $fila = $result->fetch_array();
+
+        $db->close();
+
+        if($fila['n'] == 0){
+            return User::LoginBadEmail;
+        }
+
+        return User::LoginBadPass;
     }
 }
 
