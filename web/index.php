76a70b9b0698405d0543e7e6fa64869b0d88cedc
David Canales Parra <canalesparradavid@gmail.com>
2023-03-22 17:53:52 +0100

    Pasada construccion de los elementos comunes de la pagina a la clase Page

--- web/index.php
+++ web/index.php
@@ -8,19 +8,6 @@ include_once "pages/RewardsPage.php";
 include_once "pages/SignupPage.php";
 include_once "pages/TasksPage.php";
 
-// Cargo las plantillas de los elementos de la pagina web
-$index_template = file_get_contents("templates/index.html");
-$header_template = file_get_contents("templates/header.html");
-$common_css_styles_template = file_get_contents("templates/common_css_styles.html");
-$footer_template = file_get_contents("templates/footer.html");
-
-$page_contents = $index_template;
-
-// Sustituyo cada  elemento por la plantilla correspondiente
-$page_contents = str_replace("<!-- HEADER -->", $header_template, $page_contents);
-$page_contents = str_replace("<!-- COMMON-CSS-STYLES -->", $common_css_styles_template, $page_contents);
-$page_contents = str_replace("<!-- FOOTER -->", $footer_template, $page_contents);
-
 // Calculo la pagina que se debe mostrar
 $page_name = isset($_GET['p'])? $_GET['p'] : 'main';
 switch($page_name){
@@ -42,12 +29,6 @@ switch($page_name){
      default:
         $page = new MainPage();
 }
-
-// Sustituyo el contenido principal de la pagina
-$page_contents = str_replace("<!-- CSS-STYLES -->", $page->getStylesContent(), $page_contents);
-$page_contents = str_replace("<!-- PRE-SCRIPTS -->", $page->getPreScriptsContent(), $page_contents);
-$page_contents = str_replace("<!-- BODY -->", $page->getContent(), $page_contents);
-$page_contents = str_replace("<!-- POST-SCRIPTS -->", $page->getPostScriptsContent(), $page_contents);
 
 // Cargo el contenido en pantalla
 echo $page_contents;
--- web/pages/Page.php
+++ web/pages/Page.php
@@ -5,10 +5,38 @@ include_once  "./models/User.php";
 abstract class Page{
     protected $user;
     protected $root_dir;
+    private $generated_html;
 
     function __construct(){
+        // Cargo la sesion del usuario y establezco la ruta por defecto de las plantillas
         $this->user = User::restoreSession();
         $this->root_dir = $this->setRootDir();
+
+        // Creo el contenido de la pagina web
+            // Cargo las plantillas de los elementos de la pagina web
+            $index_template = file_get_contents("templates/index.html");
+            $header_template = file_get_contents("templates/header.html");
+            $common_css_styles_template = file_get_contents("templates/common_css_styles.html");
+            $footer_template = file_get_contents("templates/footer.html");
+
+            $page_contents = $index_template;
+
+            // Sustituyo cada  elemento por la plantilla correspondiente
+            $page_contents = str_replace("<!-- HEADER -->", $header_template, $page_contents);
+            $page_contents = str_replace("<!-- COMMON-CSS-STYLES -->", $common_css_styles_template, $page_contents);
+            $page_contents = str_replace("<!-- FOOTER -->", $footer_template, $page_contents);
+
+            // Sustituyo el contenido principal de la pagina
+            $page_contents = str_replace("<!-- CSS-STYLES -->", $this->getStylesContent(), $page_contents);
+            $page_contents = str_replace("<!-- PRE-SCRIPTS -->", $this->getPreScriptsContent(), $page_contents);
+            $page_contents = str_replace("<!-- BODY -->", $this->getContent(), $page_contents);
+            $page_contents = str_replace("<!-- POST-SCRIPTS -->", $this->getPostScriptsContent(), $page_contents);
+
+        $this->generated_html = $page_contents;
+    }
+
+    public function loadPage(){
+        return $this->generated_html;
     }
 
     public function getPreScriptsContent(){
